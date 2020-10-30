97b7cd136863f3755e22182842a1c4ebc5c4197e
Mikolaj <smgpwwe@wp.pl>
2020-10-30 13:13:44 +0100

    li

--- src/components/hamburger/LiHamburger.js
+++ src/components/hamburger/LiHamburger.js
@@ -8,8 +8,8 @@ const LiHamburger = () => {
         <li><Link className="link" to="/">Home</Link><i className="fas fa-home"></i></li>
         <li><Link className="link" to="/search-recipe">Search recipes</Link><i className="fas fa-search"></i></li>
         <li><Link className="link" to="/tasks">See user tasks</Link><i className="fas fa-thumbtack"></i></li>
-        <li><a className="link" href="/#about">About us</a><i className="fas fa-question-circle"></i></li>
-        <li><a className="link" href="/#gallery">Gallery</a><i className="fas fa-camera"></i></li>
+        <li><a className="link" href="#about">About us</a><i className="fas fa-question-circle"></i></li>
+        <li><a className="link" href="#gallery">Gallery</a><i className="fas fa-camera"></i></li>
         <li><Link className="link" to="/article">Read more!<i className="fas fa-file"></i></Link></li>
         </>
     )
